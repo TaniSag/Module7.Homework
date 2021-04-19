215cc71c165c88cd0165ccc0bf82d3a085c06c4e
TaniSag <72441543+TaniSag@users.noreply.github.com>
2021-04-19 15:46:44 +0300

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,8 +1,8 @@
 package Module7.Homework;
 
-public class Rectangle implements Shape {
+public class Rectangle extends Shape {
     @Override
     public void shape() {
-        System.out.println("The figure is Rectangle");
+        System.out.println("The figure Rectangle has four sides");
     }
 }
