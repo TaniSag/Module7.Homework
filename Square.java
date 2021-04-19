44a22323805393f0155b3c61ca38232c782d0c81
TaniSag <72441543+TaniSag@users.noreply.github.com>
2021-04-19 15:47:43 +0300

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,8 +1,8 @@
 package Module7.Homework;
 
-public class Square implements Shape {
+public class Square extends Shape {
     @Override
     public void shape() {
-        System.out.println("The figure is Square");
+        System.out.println("The figure Square has four equals sides");
     }
 }
